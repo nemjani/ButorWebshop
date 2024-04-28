34420022b21888da0bbc2a15a3e71b2c11ca60b5
Némedi János <3.nemedi.janos@gmail.com>
2024-04-28 18:15:45 +0200

    pages and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'butor-webshop';
+export class AppComponent implements OnInit {
+  page = '';
+
+  routes: Array<string> = [];
+
+  constructor(private router: Router) {
+    //paraméter adattag ,ugyanaz mintha a konstruktor fölött létrehoztam volna és a k.ban példányosítottam volna.
+  }
+
+  ngOnInit() {
+    this.routes = this.router.config.map(conf => conf.path) as string[];
+
+    //reaktív programozás -> Observable
+    //feliratkozunk egy eseményre amit a pipe-al szűrünk
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((evts: any) => {
+      //console.log(evts);
+      const currentPage = (evts.urlAfterRedirects as string).split('/')[1] as string;
+      if (this.routes.includes(currentPage)) {
+        this.page = currentPage;
+      }
+    });
+  }
+
+  changePage(selectedPage: string) {
+    // this.page = selectedPage;
+    this.router.navigateByUrl(selectedPage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { MainComponent } from './pages/main/main.component';
+//import { ProductComponent } from './pages/product/product.component';
+//import { ContactComponent } from './pages/contact/contact.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //MainComponent,
+    // ProductComponent,
+    //ContactComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
